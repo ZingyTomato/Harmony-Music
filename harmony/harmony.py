c02546cf3ede01a481382ff9118ca8e2e9e2a75c
Zingy Tomato <zingytomato@tutanota.com>
2022-05-18 19:07:38 +0530

    Improve program description.

--- harmony/harmony.py
+++ harmony/harmony.py
@@ -3,9 +3,9 @@ import songs
 import functions
 import termcolor
 
-parser = argparse.ArgumentParser(description="An open souce video/music streamer based on MPV and piped.")
+parser = argparse.ArgumentParser(description="An open souce CLI music streamer based on MPV.", prog="harmony")
 
-parser.add_argument('SONG_NAME', help="Searches for songs based on the query. Example: harmony 2step Ed Sheeran", type=str, nargs="*")
+parser.add_argument('SONG_NAME', help="Name of the song to search for. Example: harmony 2step Ed Sheeran", type=str, nargs="*")
 
 args = parser.parse_args()
 
