b94b7dbb6a3b76f7fe37ffa5c603237d4c58c473
Zingy Tomato <zingytomato@tutanota.com>
2022-05-27 22:51:46 +0530

    Improve readability of lyrics.
    Lyrics are now all in capitals and have a music icon next to them.

--- harmony/functions.py
+++ harmony/functions.py
@@ -209,7 +209,7 @@ def lyricsToVtt(query):
             file.write("\n")
             file.write(f"{parseTime(v['seconds'])} --> {parseTime(data[i + 1]['seconds'])}")
             file.write("\n")
-            file.write(f"{v['lyrics']}")
+            file.write(f"♪ {v['lyrics']} ♪".upper())
             file.write("\n")
           except IndexError:
             return
