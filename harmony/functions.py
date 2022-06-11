9f58fc218479d8d5f125bbe6b63b3a0d85248759
Zingy Tomato <zingytomato@tutanota.com>
2022-06-11 13:56:40 +0530

    Improve accuracy of lyrics and remove videos.
    The **--video** flag has officially been discontinued. Code cleanup in several areas.

--- harmony/functions.py
+++ harmony/functions.py
@@ -4,7 +4,6 @@ import time
 import requests
 import json
 import songs
-import videos
 import re
 import sys
 import signal
@@ -29,10 +28,6 @@ URL_REGEX = r"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&:
 
 SEARCH_URL = "https://saavn.me"
 
-PIPEDAPI_URL = "https://pipedapi.kavin.rocks"
-
-PIPED_URL = "https://piped.kavin.rocks"
-
 LYRICS_API = "https://api.textyl.co/api"
 
 SUB_FILE = ""
@@ -40,7 +35,6 @@ SUB_FILE = ""
 def emptyQueue():
     item_list.clear()
     queue_list.clear()
-    audio_list.clear()
     title_list.clear()
     author_list.clear()
 
@@ -81,9 +75,14 @@ def queueIsEmpty():
     return 
 
 def fixFormatting(text):
-      inital_text = html.unescape(f'{text}')
-      final_text = re.sub("[\"\']", "", inital_text)
-      return final_text
+    inital_text = html.unescape(f'{text}')
+    final_text = re.sub("[\"\']", "", inital_text)
+    final_title = final_text.split(',', 1)[0]
+    return final_title
+
+def removeBrackets(text):
+    final_text = re.sub("[\(\[].*?[\)\]]", "", text)
+    return final_text
 
 def isExplicit(value):
     if value == 1:
@@ -96,25 +95,33 @@ def emptyInput():
     print(colored("\nPlease enter the name of a song!", 'red', attrs=['bold']))
     return
 
+def queueMessageSongs():
+    queuemsg = print(colored("\nPlaying songs in the Queue", 'cyan', attrs=['bold']) + colored(' (Q)uit, (L)oop, (J) Disable Lyrics', 'red')) 
+    return queuemsg
+
 def clearScreen():
     return os.system("clear")
+
+def removeSongs():
+    try:
+        item_list.pop(0)
+        if len(item_list) == 0:
+            return
+        return clearScreen(), queueMessageSongs(), showQueue()
+    except:
+        return 
 
 def showQueue():
     if len(item_list) == 0:
       return queueIsEmpty()
     else:
-      show_queue = print(f"\ns".join([f"\n{colored(i, 'green')}. {fixFormatting(track)}" for i, track in enumerate((item_list), start=1)]))     
+      show_queue = print(f"\n".join([f"\n{colored(i, 'green')}. {fixFormatting(track)}" for i, track in enumerate((item_list), start=1)]))     
       return 
 
 def showResults(query, result):
     info = print(colored("Results for", 'red') + colored(f" {query}\n", 'cyan', attrs=['bold']))
-    lists = print(f"\n".join([f"{colored(i, 'green')}. {colored(fixFormatting(item['name']), 'red', attrs=['bold'])} - {colored(fixFormatting(item['primaryArtists'].split(',', 1)[0]), 'cyan', attrs=['bold'])} ({time.strftime('%M:%S',time.gmtime(int(item['duration'])))}) {isExplicit(int(item['explicitContent']))}" for i, item in enumerate((result['results']), start=1)]))
+    lists = print(f"\n".join([f"{colored(i, 'green')}. {colored(fixFormatting(item['name']), 'red', attrs=['bold'])} - {colored(fixFormatting(item['primaryArtists']), 'cyan', attrs=['bold'])} ({time.strftime('%M:%S',time.gmtime(int(item['duration'])))}) {isExplicit(int(item['explicitContent']))}" for i, item in enumerate((result['results']), start=1)]))
     return songs.pickTrack(query, result)
-
-def showResultsVideos(query, result):
-    info = print(colored("Results for", 'red') + colored(f" {query}\n", 'cyan', attrs=['bold']))
-    lists = print(f"\n".join([f"{colored(i, 'green')}. {colored(fixFormatting(item['title']), 'red', attrs=['bold'])} - {colored(fixFormatting(item['uploaderName']), 'cyan', attrs=['bold'])} ({time.strftime('%M:%S',time.gmtime(item['duration']))})" for i, item in enumerate((result['items']), start=1)]))
-    return videos.pickVideo(query, result)
 
 def forceQuit(signum, frame):
     print(colored("\nForce quit by user.", 'red', attrs=['bold']))
@@ -142,54 +149,25 @@ def getSongs(query):
     print(colored("Loading results...", 'green', attrs=['bold']), end="\r")
     time.sleep(0.5)
     return showResults(query, searchjson)
-
-def getVideos(query):
-    clearScreen()
-    print(colored("\nSearching for videos...", 'cyan', attrs=['bold']))
-    searchurl = requests.request("GET", f"{PIPEDAPI_URL}/search?q={query}&filter=videos", headers=headers).text.encode()
-    searchjson = json.loads(searchurl)
-    if len(searchjson['items']) == 0:
-        return noResults(query), videos.searchVideos()
-    print(colored("\nFound results!", 'green', attrs=['bold']), end="\r")
-    time.sleep(0.5)
-    print(colored("Loading results...", 'green', attrs=['bold']), end="\r")
-    time.sleep(0.5)
-    return showResultsVideos(query, searchjson)
     
 def playTracks():
     if len(item_list) == 0:
       return queueIsEmpty()
     clearScreen()
-    queuemsg = print(colored("\nPlaying songs in the Queue", 'cyan', attrs=['bold']) + colored(' (Q)uit, (L)oop, (J) Disable Lyrics\n', 'red')) 
-    show_queue = print(f"\n".join([f"{colored(i, 'green')}. {fixFormatting(track)} \n" for i, track in enumerate((item_list), start=1)]))     
-    print(colored("Launching MPV and searching for Lyrics...", 'green', attrs=['bold']), end="\r")
+    queueMessageSongs()
+    showQueue()
+    print(colored("\nLaunching MPV and searching for Lyrics...", 'green', attrs=['bold']), end="\r")
     for track, title, author in zip(queue_list, title_list, author_list):
-        lyrics.searchLyrics(f"{title} - {author}")
+        lyrics.searchLyrics(f"{removeBrackets(title)} - {fixFormatting(author)}")
         os.system(f"mpv --no-video --term-osd-bar --no-resume-playback {SUB_FILE} --term-playing-msg='{fixFormatting(colored(title, 'red'))} - {fixFormatting(colored(author, 'cyan'))}' '{track}'")
         removeSubs()
     return emptyQueue(), removeSubs(), songs.searchSongs()
-
-def playVideos():
-    if len(item_list) == 0:
-      return queueIsEmpty()
-    clearScreen()
-    queuemsg = print(colored("\nPlaying videos in the Queue", 'cyan', attrs=['bold']) + colored(' (Q)uit, (L)oop\n', 'red')) 
-    show_queue = print(f"\n".join([f"{colored(i, 'green')}. {fixFormatting(track)} \n" for i, track in enumerate((item_list), start=1)]))     
-    print(colored("Launching MPV...", 'green', attrs=['bold']), end="\r")
-    play_videos = [os.system(f"mpv --term-osd-bar --force-media-title='{fixFormatting(title)}' --no-resume-playback '{track}' --audio-file='{audio}' ") for track, audio, title in zip(queue_list, audio_list, title_list)]
-    return emptyQueue(), videos.searchVideos()
 
 def playTracksURL(url):
     clearScreen()
     print(colored("Launching MPV...", 'green', attrs=['bold']), end="\r")
     play_videos = os.system(f"mpv --no-video --term-osd-bar --no-resume-playback {url} ")
     return songs.searchSongs()
-
-def playVideosURL(url):
-    clearScreen()
-    print(colored("Launching MPV...", 'green', attrs=['bold']), end="\r")
-    play_videos = os.system(f"mpv --no-resume-playback {url} ")
-    return videos.searchVideos()
 
 def addSongs(videoid, title, author, duration, explicit):
     queue_list.append(videoid)
@@ -198,19 +176,5 @@ def addSongs(videoid, title, author, duration, explicit):
     item_list.append(f"{colored(title, 'red')} - {colored(author, 'cyan')} ({duration}) {explicit}")
     added = print(colored(f"\n{fixFormatting(title)} - ", 'cyan') + colored(f"{fixFormatting(author)}", 'red') + colored(" has been added to the queue.", 'green'))
     return songs.searchSongs()
-
-def addVideos(videoid, title, author):
-    print(colored("\nGathering info...", 'green', attrs=['bold']), end="\r")
-    videoid = videoid.replace("/watch?v=", "")
-    searchurl = requests.request("GET", f"{PIPEDAPI_URL}/streams/{videoid}", headers=headers).text.encode()
-    searchjson = json.loads(searchurl)
-    videourl = searchjson['videoStreams'][0]['url']
-    audiourl = searchjson['audioStreams'][0]['url']
-    queue_list.append(videourl)
-    audio_list.append(audiourl)
-    title_list.append(title)
-    item_list.append(f"{colored(title, 'red')} - {colored(author, 'cyan')}")
-    added = print(colored(f"{fixFormatting(title)} - ", 'cyan') + colored(f'{fixFormatting(author)}', 'red') + colored(" has been added to the queue.", 'green'))
-    return videos.searchVideos()
 
 signal.signal(signal.SIGINT, forceQuit)
